5512e1499a22ce662e8225568dab427104c7660f
M4theus1 <matheus.silva.alves2909@gmail.com>
2025-07-01 21:15:56 -0300

    Primeiro Commit

---
+++ src/main/java/com/desafio/poi_service/PoiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.poi_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/poi_service/PoiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.poi_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
